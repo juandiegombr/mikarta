28786089dbff5230d8687d8f3cccaea8fffa3fe4
Juan Diego <juandiego.mbr@gmail.com>
2020-05-12 19:07:07 +0200

    qr-scanner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  // useParams,
+} from "react-router-dom"
+import QrReader from 'react-qr-reader'
+
+import './App.css'
+
+const Home = () => {
+  return (
+    <main className="home-page">
+      <section className="category-container">
+        <div className="category-wrapper">
+          <Link to="/category/1" className="category-button">Entrantes</Link>
+        </div>
+        <div className="category-wrapper">
+          <Link to="/category/2" className="category-button">Hamburguesas</Link>
+        </div>
+        <div className="category-wrapper">
+          <Link to="/category/3" className="category-button">Bocadillos</Link>
+        </div>
+        <div className="category-wrapper">
+          <Link to="/category/4" className="category-button">Refrescos</Link>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+const Category = () => {
+  // const { categoryId } = useParams()
+
+  return (
+    <Fragment>
+      <header className="header">
+        <Link to="/">Atrás</Link>
+      </header>
+      <main className="category-page">
+        <section className="product-container">
+          <div className="product-wrapper">
+            <div className="product-button">Bravas</div>
+          </div>
+          <div className="product-wrapper">
+            <div className="product-button">Morro</div>
+          </div>
+          <div className="product-wrapper">
+            <div className="product-button">Calamares</div>
+          </div>
+        </section>
+      </main>
+    </Fragment>
+  )
+}
+
+const Scanner = () => {
+  const handleError = (error) => {
+    console.log('error', error)
+  }
+  const handleScan = qr => {
+    console.log(qr)
+  }
+  return (
+    <QrReader
+      facingMode="user"
+      delay={300}
+      onError={handleError}
+      onScan={handleScan}
+      style={{ width: '100%' }}
+    />
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/category/:categoryId">
+            <Category />
+          </Route>
+          <Route path="/">
+            <Scanner />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
