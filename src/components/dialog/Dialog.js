b60b2bc6f682641711b0111c089d33f2a61ba934
Juan Diego <juandiego.mbr@gmail.com>
2020-05-15 20:54:24 +0200

    Disable scroll

--- src/components/dialog/Dialog.js
+++ src/components/dialog/Dialog.js
@@ -5,11 +5,21 @@ import './Dialog.css'
 
 const modalRoot = document.getElementById('dialog')
 
+const blockScroll = () => document.body.style.overflowY = 'hidden'
+const unblockScroll = () => document.body.style.overflowY = 'visible'
+
 const Dialog = ({ visible, closeDialog, children }) => {
   if (!visible) return null
 
+  blockScroll()
+
+  const closeDialogSelf = () => {
+    unblockScroll()
+    closeDialog()
+  }
+
   return createPortal(
-    <div className="dialog-overlay" onClick={ closeDialog }>
+    <div className="dialog-overlay" onClick={ closeDialogSelf }>
       <div className="dialog-wrapper">
         <div role="dialog" className="dialog-content">
           { children }
