21c49f5eb47e1522693cc2c9862a15403413bfa2
Juan Diego <juandiego.mbr@gmail.com>
2020-05-18 22:13:58 +0200

    Update products range

--- src/app/product/client.js
+++ src/app/product/client.js
@@ -28,7 +28,7 @@ const getAllByCategoryId = async (restaurantId, id) => {
   if (localProducts) return JSON.parse(localProducts).filter(product => product.categoryId === id)
 
   const idSheets = sessionStorage.getItem(KEYS.GAPI_REF) || await ReferenceClient.getReference(restaurantId)
-  const data = await Spreadsheets.getData(idSheets, 'productos!A3:K100')
+  const data = await Spreadsheets.getData(idSheets, 'productos!A3:R200')
   const products = productsSerializer(data)
   sessionStorage.setItem(KEYS.PRODUCTS, JSON.stringify(products))
   return products.filter(product => product.categoryId === id)
